a7fd85b215a166d83705f91eea27665e133b4025
Jerome Czachor <jerome.czachor@g2-inc.com>
2018-10-18 14:14:35 -0400

    Submodule init update

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
+import sys
+
 try:
     from . import sb_utils
-except Exception as e:
-    print(e)
+except Exception:
     import sb_utils
 
-__all__ = [m for m in sb_utils.__all__]
+__all__ = []
 
+for m in sb_utils.__all__:
+    __all__.append(m)
+    setattr(sys.modules[__name__], m, getattr(sb_utils, m))
 
 
